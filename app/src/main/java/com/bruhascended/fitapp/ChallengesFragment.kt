087303686f6b6aa901ee9393ddc1a51a6f646cc6
Gurjyot Singh Bhullar <gjbhullar16@gmail.com>
2021-04-21 15:06:42 +0530

    Set isScrollable to false for challenges fragment

--- app/src/main/java/com/bruhascended/fitapp/ChallengesFragment.kt
+++ app/src/main/java/com/bruhascended/fitapp/ChallengesFragment.kt
@@ -5,14 +5,18 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.bruhascended.fitapp.databinding.FragmentChallengesBinding
 
 class ChallengesFragment : Fragment() {
+    private lateinit var binding:FragmentChallengesBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_challenges, container, false)
+        binding = FragmentChallengesBinding.inflate(inflater)
+        binding.nestedScrollView.isNestedScrollingEnabled = false
+        return binding.root
     }
 }
