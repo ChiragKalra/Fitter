a2da0a16ef8a1b2b945c11e77f684f2b1f1e4807
Chirag Kalra <arlakgarihc@gmail.com>
2021-05-25 00:33:07 +0530

    added camera preview to CaptureFoodActivity.kt

--- app/src/main/java/com/bruhascended/fitapp/capturefood/CaptureFoodActivity.kt
+++ app/src/main/java/com/bruhascended/fitapp/capturefood/CaptureFoodActivity.kt
@@ -1,20 +1,113 @@
 package com.bruhascended.fitapp.capturefood
 
+import android.Manifest
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import android.widget.Toast
+import androidx.camera.core.CameraSelector
+import androidx.camera.core.ImageCapture
+import androidx.camera.core.Preview
+import androidx.camera.lifecycle.ProcessCameraProvider
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import androidx.databinding.DataBindingUtil
 import com.bruhascended.fitapp.R
 import com.bruhascended.fitapp.databinding.ActivityCaptureFoodBinding
 import com.bruhascended.fitapp.util.setupToolbar
+import java.io.File
+import java.util.concurrent.ExecutorService
+import java.util.concurrent.Executors
+
+typealias LumaListener = (luma: Double) -> Unit
 
 class CaptureFoodActivity : AppCompatActivity() {
 
+    companion object {
+        private const val TAG = "CameraXBasic"
+        private const val REQUEST_CODE_PERMISSIONS = 10
+        private val REQUIRED_PERMISSIONS = arrayOf(Manifest.permission.CAMERA)
+    }
+
     private lateinit var binding: ActivityCaptureFoodBinding
+
+    private lateinit var cameraExecutor: ExecutorService
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = DataBindingUtil.setContentView(this, R.layout.activity_capture_food)
         setupToolbar(binding.toolbar, mTitle = "", home = true)
 
+        // Request camera permissions
+        if (allPermissionsGranted()) {
+            startCamera()
+        } else {
+            ActivityCompat.requestPermissions(
+                this, REQUIRED_PERMISSIONS, REQUEST_CODE_PERMISSIONS)
+        }
+
+        cameraExecutor = Executors.newSingleThreadExecutor()
+    }
+
+    private fun takePhoto() {}
+
+    private fun startCamera() {
+        val cameraProviderFuture = ProcessCameraProvider.getInstance(this)
+
+        cameraProviderFuture.addListener({
+            // Used to bind the lifecycle of cameras to the lifecycle owner
+            val cameraProvider: ProcessCameraProvider = cameraProviderFuture.get()
+
+            // Preview
+            val preview = Preview.Builder()
+                .build()
+                .also {
+                    it.setSurfaceProvider(binding.viewFinder.surfaceProvider)
+                }
+
+            // Select back camera as a default
+            val cameraSelector = CameraSelector.DEFAULT_BACK_CAMERA
+
+            try {
+                // Unbind use cases before rebinding
+                cameraProvider.unbindAll()
+
+                // Bind use cases to camera
+                cameraProvider.bindToLifecycle(
+                    this, cameraSelector, preview)
+
+            } catch(exc: Exception) {
+                Log.e(TAG, "Use case binding failed", exc)
+            }
+
+        }, ContextCompat.getMainExecutor(this))
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        if (requestCode == REQUEST_CODE_PERMISSIONS) {
+            if (allPermissionsGranted()) {
+                startCamera()
+            } else {
+                Toast.makeText(this,
+                    "Permissions not granted by the user.",
+                    Toast.LENGTH_SHORT).show()
+                finish()
+            }
+        }
+    }
+
+    private fun allPermissionsGranted() = REQUIRED_PERMISSIONS.all {
+        ContextCompat.checkSelfPermission(
+            baseContext, it) == PackageManager.PERMISSION_GRANTED
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        cameraExecutor.shutdown()
     }
 }
---
+++ classifier/src/main/java/com/bruhascended/classifier/FoodImageClassifier.kt
@@ -0,0 +1,66 @@
+package com.bruhascended.classifier
+
+import android.content.Context
+import android.graphics.Bitmap
+import com.bruhascended.classifier.ml.AiyVisionClassifierFoodV1
+import org.tensorflow.lite.DataType
+import org.tensorflow.lite.gpu.CompatibilityList
+import org.tensorflow.lite.support.image.ImageProcessor
+import org.tensorflow.lite.support.image.TensorImage
+import org.tensorflow.lite.support.image.ops.ResizeOp
+import org.tensorflow.lite.support.model.Model
+import kotlin.math.max
+
+
+class FoodImageClassifier (
+    private val context: Context
+) {
+
+    private var model: AiyVisionClassifierFoodV1
+    private var imageProcessor: ImageProcessor
+
+    init {
+        val compatList = CompatibilityList()
+        val device = if (compatList.isDelegateSupportedOnThisDevice)
+            Model.Device.GPU else Model.Device.CPU
+
+        val options = Model.Options.Builder()
+            .setNumThreads(6)
+            .setDevice(device)
+            .build()
+
+        model = AiyVisionClassifierFoodV1.newInstance(context, options)
+
+        imageProcessor = ImageProcessor.Builder()
+            .add(
+                ResizeOp(
+                    224,
+                    224,
+                    ResizeOp.ResizeMethod.BILINEAR
+                )
+            )
+            .build()
+    }
+
+
+    fun fetchResults (bitmap: Bitmap): Array<String> {
+
+        var tensorImage = TensorImage(DataType.UINT8)
+        tensorImage.load(bitmap)
+        tensorImage = imageProcessor.process(tensorImage)
+
+        // Runs model inference and gets result.
+        val outputs = model.process(tensorImage)
+        val probability = outputs.probabilityAsCategoryList
+        probability.sortByDescending { it.score }
+
+        return Array(max(5, probability.size)) {
+            probability[it].label
+        }
+    }
+
+    fun close() {
+        // Releases model resources if no longer used.
+        model.close()
+    }
+}
