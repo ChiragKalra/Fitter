d4b37d7bd214e4d69ba2e7d323c8be63bf1c09a5
Gurjyot Singh Bhullar <gjbhullar16@gmail.com>
2021-06-09 10:36:59 +0530

    Added AddFoodActivity ViewModel

--- app/src/main/java/com/bruhascended/fitapp/ui/addfood/AddFoodActivityViewModel.kt
+++ app/src/main/java/com/bruhascended/fitapp/ui/addfood/AddFoodActivityViewModel.kt
@@ -1,4 +1,41 @@
 package com.bruhascended.fitapp.ui.addfood
 
-class AddFoodActivityViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.bruhascended.api.models.foods.Food
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers.IO
+import kotlinx.coroutines.launch
+import java.sql.Date
+import java.text.DateFormat
+import java.text.SimpleDateFormat
+import java.util.*
+
+class AddFoodActivityViewModel : ViewModel() {
+
+    private var _FoodName = MutableLiveData<String>()
+    var FoodName: LiveData<String> = _FoodName
+
+    private var _FoodEnergy = MutableLiveData<String>()
+    var FoodEnergy: LiveData<String> = _FoodEnergy
+
+    private var _EnergyUnit = MutableLiveData<String>()
+    var EnergyUnit: LiveData<String> = _EnergyUnit
+
+    val setDate: String = SimpleDateFormat("dd/MM/yyyy", Locale.getDefault()).format(Date())
+    val setTime: String = SimpleDateFormat("HH:mm", Locale.getDefault()).format(Date())
+
+    fun setData(food: Food) {
+        CoroutineScope(IO).launch {
+            for (i in food.foodNutrients) {
+                if (i.nutrientId == 1008) {
+                    _FoodName.postValue(food.description.lowercase())
+                    _FoodEnergy.postValue(i.value.toString())
+                    _EnergyUnit.postValue(i.unitName)
+                    break
+                }
+            }
+        }
+    }
 }
