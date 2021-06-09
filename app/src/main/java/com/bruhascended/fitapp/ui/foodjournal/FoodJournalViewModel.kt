e2a4688d5f337c5df30fb0d4a2f07a00ddba99e7
Chirag Kalra <arlakgarihc@gmail.com>
2021-06-10 03:44:46 +0530

    remove test code

--- app/src/main/java/com/bruhascended/fitapp/ui/foodjournal/FoodJournalViewModel.kt
+++ app/src/main/java/com/bruhascended/fitapp/ui/foodjournal/FoodJournalViewModel.kt
@@ -14,36 +14,4 @@ class FoodJournalViewModel (mApp: Application) : AndroidViewModel(mApp) {
     private val foodEntryRepository by FoodEntryRepository.Companion.Delegate(mApp)
 
     val foodEntries = foodEntryRepository.loadConsumedFoodEntries()
-
-    init {
-        CoroutineScope(Dispatchers.IO).launch {
-            var id = foodEntryRepository.writeEntry(
-                Food("Apple", healthRating = 4),
-                Entry(
-                    120.0,
-                    1.0,
-                    QuantityType.Units,
-                    Calendar.getInstance().timeInMillis
-                )
-            )
-            id = foodEntryRepository.writeEntry(
-                Food("Corn Soup", healthRating = 5),
-                Entry(
-                    40.0,
-                    500.0,
-                    QuantityType.MilliLiters,
-                    Calendar.getInstance().timeInMillis
-                )
-            )
-            id = foodEntryRepository.writeEntry(
-                Food("Fries", healthRating = 1),
-                Entry(
-                    800.0,
-                    200.0,
-                    QuantityType.Grams,
-                    Calendar.getInstance().timeInMillis
-                )
-            )
-        }
-    }
 }
