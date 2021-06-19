df0856b2350df5498756b87bd8f04f3a4e49d80c
Gurjyot Singh Bhullar <gjbhullar16@gmail.com>
2021-06-20 00:04:13 +0530

    Added CustomFood Activity

---
+++ app/src/main/java/com/bruhascended/fitapp/ui/addFood/AddCustomFood.kt
@@ -0,0 +1,12 @@
+package com.bruhascended.fitapp.ui.addFood
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.bruhascended.fitapp.R
+
+class AddCustomFood : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_add_custom_food)
+    }
+}
