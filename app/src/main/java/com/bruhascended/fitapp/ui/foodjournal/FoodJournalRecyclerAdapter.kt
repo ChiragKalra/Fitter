230d56929f11f6227aaa6eabd01ebc3771e788a3
Chirag Kalra <arlakgarihc@gmail.com>
2021-06-20 16:37:40 +0530

    updated FoodJournal UI and fixed minor bugs

--- app/src/main/java/com/bruhascended/fitapp/ui/foodjournal/FoodJournalRecyclerAdapter.kt
+++ app/src/main/java/com/bruhascended/fitapp/ui/foodjournal/FoodJournalRecyclerAdapter.kt
@@ -11,6 +11,7 @@ import com.bruhascended.db.food.types.NutrientType
 import com.bruhascended.db.food.types.QuantityType
 import com.bruhascended.db.food.types.QuantityType.Companion.doubleToString
 import com.bruhascended.db.R.string.calorie_count
+import com.bruhascended.fitapp.R
 import com.bruhascended.fitapp.databinding.ItemFoodEntryBinding
 import com.bruhascended.fitapp.databinding.ItemSeparatorFoodentryBinding
 import com.bruhascended.fitapp.ui.foodjournal.FoodJournalRecyclerAdapter.FoodEntryItemHolder
@@ -22,9 +23,6 @@ class FoodJournalRecyclerAdapter (
 ): PagingDataAdapter<DateSeparatedItem, FoodEntryItemHolder> (
     DateSeparatedItemComparator()
 ) {
-
-    private var mOnItemClickListener: ((foodEntry: FoodEntry) -> Unit)? = null
-
     class FoodEntryItemHolder (
         root: View,
         val itemBinding: ItemFoodEntryBinding? = null,
@@ -32,6 +30,8 @@ class FoodJournalRecyclerAdapter (
     ) : RecyclerView.ViewHolder(root) {
         var layoutNutrientsWrapHeight = root.context.toPx(36).toInt()
     }
+
+    private var mOnItemClickListener: ((foodEntry: FoodEntry) -> Unit)? = null
 
     fun setOnItemClickListener (listener: ((foodEntry: FoodEntry) -> Unit)?) {
         mOnItemClickListener = listener
@@ -59,83 +59,116 @@ class FoodJournalRecyclerAdapter (
         }
     }
 
+    private fun ItemSeparatorFoodentryBinding.presentSeparator(
+        item: DateSeparatedItem,
+    ) {
+        if (item.separator == null) return
+
+        textviewDate.text = DateTimePresenter(mContext, item.separator.time).fullDate
+
+        textviewCalories.text = mContext.getString(
+            calorie_count,
+            doubleToString(item.totalCalories)
+        )
+        // TODO: Set Using User Preference
+        progressbarCalories.apply {
+            progress = 0f
+            progressMax = 1800f
+            setProgressWithAnimation(item.totalCalories.toFloat())
+        }
+
+        item.totalNutrients.forEach { (type, value) ->
+            if (type == null) return@forEach
+            when (type) {
+                NutrientType.Protein -> textviewProteinGram
+                NutrientType.Carbs -> textviewCarbsGram
+                NutrientType.Fat -> textviewFatGram
+            }.text = QuantityType.Gram.toString(mContext, value)
+
+            when (type) {
+                NutrientType.Protein -> progressbarProtein
+                NutrientType.Carbs -> progressbarCarbs
+                NutrientType.Fat -> progressbarFat
+            }.apply {
+                // TODO: Set Using User Preference
+                progressMax = 100f
+                progress = 0f
+                setProgressWithAnimation(value.toFloat(), AnimationDuration.VERY_LONG.ms)
+            }
+        }
+    }
+
+
+    private fun ItemFoodEntryBinding.presentItem(
+        foodEntry: FoodEntry,
+        holder: FoodEntryItemHolder,
+        position: Int
+    ) {
+        val food = foodEntry.food
+        val entry = foodEntry.entry
+
+        textviewMeal.text = entry.mealType.getString(mContext)
+        textviewCalories.text = mContext.getString(
+            calorie_count,
+            doubleToString(entry.calories)
+        )
+        textviewFoodName.text = food.foodName
+        textviewQuantity.text = entry.quantityType.toString(mContext, entry.quantity)
+
+        val weight = entry.quantity * (food.weightInfo[entry.quantityType] ?: .0)
+        food.nutrientInfo.forEach { (type, value) ->
+            if (type == null) return@forEach
+            when (type) {
+                NutrientType.Protein -> textviewProteinGram
+                NutrientType.Carbs -> textviewCarbsGram
+                NutrientType.Fat -> textviewFatGram
+            }.text = QuantityType.Gram.toString(mContext, value*weight)
+        }
+
+        root.setOnClickListener {
+            mOnItemClickListener?.invoke(foodEntry)
+        }
+
+        var expanded = false
+        layoutNutrients.layoutParams = layoutNutrients.layoutParams.apply {
+            height = 0
+        }
+        buttonExpand.scaleY = 1f
+        if (food.nutrientInfo.isEmpty()) {
+            buttonExpand.visibility = View.INVISIBLE
+        } else {
+            buttonExpand.setOnClickListener {
+                layoutNutrients.animateHeightTo(
+                    if (expanded) 0 else holder.layoutNutrientsWrapHeight
+                )
+                buttonExpand.animateScaleY(if (expanded) 1f else -1f)
+                expanded = !expanded
+            }
+        }
+
+        val isLastItem = try {
+            getItem(position + 1)?.isSeparator ?: true
+        } catch (e: IndexOutOfBoundsException) {
+            true
+        }
+        layoutRoot.setBackgroundResource(
+            if (isLastItem) R.drawable.bg_foodjournal_item_end
+            else R.drawable.bg_foodjournal_item
+        )
+        layoutRoot.layoutParams = (layoutRoot.layoutParams as ViewGroup.MarginLayoutParams).also {
+            it.bottomMargin = if (isLastItem) mContext.toPx(12).toInt() else 0
+        }
+    }
+
+
+
     override fun onBindViewHolder (holder: FoodEntryItemHolder, position: Int) {
         val item = getItem(position) ?: return
         val foodEntry = item.item
-        if (item.isSeparator && item.separator != null) {
-            holder.separatorBinding?.apply {
-                textviewDate.text = DateTimePresenter(mContext, item.separator.time).fullDate
-
-                textviewCalories.text = mContext.getString(
-                    calorie_count,
-                    doubleToString(item.totalCalories)
-                )
-                // TODO: Set Using User Preference
-                progressbarCalories.apply {
-                    progress = 0f
-                    progressMax = 1800f
-                    setProgressWithAnimation(item.totalCalories.toFloat())
-                }
-
-                item.totalNutrients.forEach { (type, value) ->
-                    if (type == null) return@forEach
-                    when (type) {
-                        NutrientType.Protein -> textviewProteinGram
-                        NutrientType.Carbs -> textviewCarbsGram
-                        NutrientType.Fat -> textviewFatGram
-                    }.text = QuantityType.Gram.toString(mContext, value)
-
-                    when (type) {
-                        NutrientType.Protein -> progressbarProtein
-                        NutrientType.Carbs -> progressbarCarbs
-                        NutrientType.Fat -> progressbarFat
-                    }.apply {
-                        // TODO: Set Using User Preference
-                        progressMax = 100f
-                        progress = 0f
-                        setProgressWithAnimation(value.toFloat(), AnimationDuration.VERY_LONG.ms)
-                    }
-                }
-            }
+        if (item.isSeparator) {
+            holder.separatorBinding?.presentSeparator(item)
         } else if (foodEntry != null) {
-            val food = foodEntry.food
-            val entry = foodEntry.entry
-            holder.itemBinding?.apply {
-                textviewMeal.text = entry.mealType.getString(mContext)
-                textviewCalories.text = mContext.getString(
-                    calorie_count,
-                    doubleToString(entry.calories)
-                )
-                textviewFoodName.text = food.foodName
-                textviewQuantity.text = entry.quantityType.toString(mContext, entry.quantity)
-
-                val weight = entry.quantity * (food.weightInfo[entry.quantityType] ?: .0)
-                food.nutrientInfo.forEach { (type, value) ->
-                    if (type == null) return@forEach
-                    when (type) {
-                        NutrientType.Protein -> textviewProteinGram
-                        NutrientType.Carbs -> textviewCarbsGram
-                        NutrientType.Fat -> textviewFatGram
-                    }.text = QuantityType.Gram.toString(mContext, value*weight)
-                }
-
-                root.setOnClickListener {
-                    mOnItemClickListener?.invoke(foodEntry)
-                }
-
-                var expanded = false
-                layoutNutrients.layoutParams = layoutNutrients.layoutParams.apply {
-                    height = 0
-                }
-                buttonExpand.scaleY = 1f
-                buttonExpand.setOnClickListener {
-                    layoutNutrients.animateHeightTo(
-                        if (expanded) 0 else holder.layoutNutrientsWrapHeight
-                    )
-                    buttonExpand.animateScaleY(if (expanded) 1f else -1f)
-                    expanded = !expanded
-                }
-            }
+            holder.itemBinding?.presentItem(foodEntry, holder, position)
         }
     }
 }
