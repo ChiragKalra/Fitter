647abc2710930bdc5bac758d8df018db9cdc3ac2
Chirag Kalra <arlakgarihc@gmail.com>
2021-07-21 15:09:40 +0530

    added PeriodicEntry.kt

--- db/src/main/java/com/bruhascended/db/activity/ActivityEntryDatabase.kt
+++ db/src/main/java/com/bruhascended/db/activity/ActivityEntryDatabase.kt
@@ -1,15 +1,20 @@
 package com.bruhascended.db.activity
 
+import androidx.lifecycle.LiveData
 import androidx.room.Database
 import androidx.room.RoomDatabase
 import androidx.room.TypeConverters
 import com.bruhascended.db.activity.daos.ActivityEntryDao
+import com.bruhascended.db.activity.daos.PeriodicEntryDao
 import com.bruhascended.db.activity.entities.ActivityEntry
+import com.bruhascended.db.activity.entities.PeriodicEntry
+import java.util.*
 
 
 @Database(
     entities = [
         ActivityEntry::class,
+        PeriodicEntry::class
      ],
     version = 1,
     exportSchema = false
@@ -19,4 +24,12 @@ import com.bruhascended.db.activity.entities.ActivityEntry
 )
 abstract class ActivityEntryDatabase : RoomDatabase() {
     abstract fun entryManager(): ActivityEntryDao
+    protected abstract fun periodicEntryManager(): PeriodicEntryDao
+
+    fun getLivePeriodicEntryOf(date: Date): LiveData<PeriodicEntry> {
+        return periodicEntryManager().getTimeRangeSumLive(
+            date.time,
+            date.time + 24*60*60*1000L
+        )
+    }
 }
---
+++ db/src/main/java/com/bruhascended/db/activity/entities/PeriodicEntry.kt
@@ -0,0 +1,51 @@
+package com.bruhascended.db.activity.entities
+
+import androidx.room.*
+import java.io.Serializable
+import kotlin.math.abs
+
+
+@Entity
+data class PeriodicEntry (
+    @PrimaryKey
+    val startTime: Long,
+    var totalCalories: Int = 0,
+    var totalDuration: Long = 0L,
+    var totalDistance: Double = .0,
+    var totalSteps: Int = 0,
+): Serializable {
+
+    operator fun plusAssign(other: ActivityEntry) {
+        totalCalories += other.calories
+        totalDuration += other.duration ?: 0L
+        totalDistance += other.distance ?: .0
+        totalSteps += other.steps ?: 0
+    }
+
+    operator fun minusAssign(other: ActivityEntry) {
+        totalCalories -= other.calories
+        totalDuration -= other.duration ?: 0L
+        totalDistance -= other.distance ?: .0
+        totalSteps -= other.steps ?: 0
+    }
+
+    override fun equals(other: Any?): Boolean {
+        if (this === other) return true
+        if (javaClass != other?.javaClass) return false
+
+        other as PeriodicEntry
+        if (startTime != other.startTime) return false
+        if (totalCalories != other.totalCalories) return false
+        if (totalDuration != other.totalDuration) return false
+        if (totalSteps != other.totalSteps) return false
+        if (totalDistance != other.totalDistance) return false
+        return true
+    }
+
+    override fun hashCode(): Int {
+        return startTime.hashCode()
+    }
+
+    val id
+        get() = abs(hashCode())
+}
