74694922c2887fceda94ed1f0cbf2db57b5e13d4
Chirag Kalra <arlakgarihc@gmail.com>
2021-12-22 01:27:12 +0530

    edamam api keys uncommented

--- api/src/main/java/com/bruhascended/api/services/FoodApi.kt
+++ api/src/main/java/com/bruhascended/api/services/FoodApi.kt
@@ -6,15 +6,15 @@ import retrofit2.Response
 import retrofit2.http.GET
 import retrofit2.http.Query
 //
-//private const val API_KEY: String = BuildConfig.EDAMAM_API_KEY // provide your own path
-//private const val APP_ID: String = BuildConfig.EDAMAM_API_ID // provide your own path
+private const val API_KEY: String = BuildConfig.EDAMAM_API_KEY // provide your own path
+private const val APP_ID: String = BuildConfig.EDAMAM_API_ID // provide your own path
 
 interface FoodApi {
 
     @GET("parser")
     suspend fun getFoodsv2(
         @Query("ingr") query: String,
-        //@Query("app_id") app_id: String = APP_ID,
-        //@Query("app_key") app_key: String = API_KEY
+        @Query("app_id") app_id: String = APP_ID,
+        @Query("app_key") app_key: String = API_KEY
     ): Response<Foodsv2Response?>
 }
